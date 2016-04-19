7aee138f592305637f70319cbd68e805cf69f559
Dean Smith <dean@zelotus.com>
2016-04-19 19:59:40 +0100

    Adding check for i p to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 ip=$1
 
+if [[ -z $ip  ]]; then
+	echo "Usage: install.sh ip_address"
+	exit 1
+fi
+
 ssh root@$ip /etc/rc.conf_mount_rw
 ssh root@$ip mkdir -p /usr/local/etc/devd
 scp carp.conf root@$ip:/usr/local/etc/devd/
